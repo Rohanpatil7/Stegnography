edba56ba98fe06e7c2c9eaa3b88b350826a44536
Rohan Patil <ytrohan00829@gmail.com>
2025-03-20 19:52:10 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,15 +1,7 @@
 from PIL import Image, ImageOps
 
 def main():
-    # img = Image.open("Assets/20250221_155239.jpg")
-    # region = img.crop((0,0,600,600))
-
-    # reg = region.transpose(Image.Transpose.ROTATE_90)
-    # img.paste(reg,(0,0,600,600))
-
-    # r,g,b = img.split()
-    # img = Image.merge("RGB",(b,g,r))
-    # img = img.rotate(90)
+  
     size = (1240,720)
     with Image.open("Assets/20250221_155239.jpg","wb") as f:
         ImageOps.contain(f,size).save("F_contaion.jpg")
